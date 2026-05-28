826cde57f8bc9f1b0dccbd98ba6fa4d8d191602b
arav-exhausted <aravprajapat1102@gmail.com>
2026-05-28 10:37:46 +0000

    new commits

--- smolproject/app/schema/product.py
+++ smolproject/app/schema/product.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel, Field
 from typing import Annotated
-
+from uuid import UUID
 
 
 class Product(BaseModel):
-    id: str
+    id: UUID
     sku: Annotated[str,Field(min_length=6,max_length=30,title="SKU",description="Stock Keeping Unit")]
     name: str
